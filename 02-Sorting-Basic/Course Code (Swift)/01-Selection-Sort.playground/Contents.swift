d8659adc25a3e980f6af3854b44388aa4ea35bd8
Wang, Shun <shun.wang@daimler.com>
2019-05-10 16:46:50 +0800

    add 01-Selection-Sort
    rename the array

---
+++ 02-Sorting-Basic/Course Code (Swift)/01-Selection-Sort.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func selectionSort(array: [Int]) -> [Int] {
+    var arr = array
+    let count = arr.count
+    for i in 0..<count {
+        var minIndex = i
+        // 寻找[i, n)区间里的最小值
+        for j in i + 1..<count {
+            if arr[j] < arr[minIndex] {
+                arr.swapAt(j, minIndex)
+            }
+        }
+    }
+    return arr
+}
+
+let a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(selectionSort(array: a))
